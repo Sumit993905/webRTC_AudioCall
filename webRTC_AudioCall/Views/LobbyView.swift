a3064b0a4cc2ab8e6496041528847b6168f07cfe
Sumit993905 <officialsumitraj993905@gmail.com>
2026-01-30 12:18:23 +0530

    api chnages

--- webRTC_AudioCall/Views/LobbyView.swift
+++ webRTC_AudioCall/Views/LobbyView.swift
@@ -16,7 +16,7 @@ struct LobbyView: View {
     @State private var callType: CallType?
 
     @StateObject private var manager =
-        WebRTCManager(signalingURL: URL(string: "https://c12d0e0d85d3.ngrok-free.app")!)
+        WebRTCManager(signalingURL: URL(string: "https://e58ad6a51b05.ngrok-free.app")!)
 
     @Environment(\.dismiss) private var dismiss
 
